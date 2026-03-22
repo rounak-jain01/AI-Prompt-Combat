4e40cb473a763b2f768a7bf2e7786541809324ba
Rounak Jain <rounakjain049@gmail.com>
2026-03-22 10:53:30 +0530

    Update Config

--- ai-prompt-combat/src/config.js
+++ ai-prompt-combat/src/config.js
@@ -1,8 +1,8 @@
 // src/config.js
 
 // Yeh automatically environment ke hisaab se URL utha lega
-// export const API_BASE_URL = "https://api.aipromptcombat.in";
-export const API_BASE_URL = "http://127.0.0.1:8000";
+export const API_BASE_URL = "https://api.aipromptcombat.in";
+// export const API_BASE_URL = "http://127.0.0.1:8000";
 
 // Console log karke check kar sakte hain ki kaunsa URL le raha hai
 console.log("Current API URL:", API_BASE_URL);
